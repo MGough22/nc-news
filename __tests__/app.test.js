3f5aabeee04824f833650ff816f6dab06c973839
MGough 22 <gentianloom@gmail.com>
2024-11-25 13:51:08 +0000

    added get endpoint, and 404 Handler

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,9 +1,19 @@
 const endpointsJson = require("../endpoints.json");
-/* Set up your test imports here */
+const testData = require("../db/data/test-data");
+const seed = require("../db/seeds/seed");
+const db = require("../db/connection");
+const request = require("supertest");
+const app = require("../app");
 
-/* Set up your beforeEach & afterAll functions here */
+beforeEach(() => {
+  return seed(testData);
+});
 
-describe.skip("GET /api", () => {
+afterAll(() => {
+  return db.end();
+});
+
+describe("GET /api", () => {
   test("200: Responds with an object detailing the documentation for each endpoint", () => {
     return request(app)
       .get("/api")
@@ -12,4 +22,18 @@ describe.skip("GET /api", () => {
         expect(endpoints).toEqual(endpointsJson);
       });
   });
+  test("200: Responds with a JSON object", () => {
+    return request(app)
+      .get("/api")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
+});
+test("404: Responds with an error message for non-existent route", () => {
+  return request(app)
+    .get("/api/non-existent-route")
+    .expect(404)
+    .then(({ body }) => {
+      expect(body).toEqual({ msg: "Route not found" });
+    });
 });
