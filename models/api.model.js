3ef649f7c524463542cfea7dbb04d818e1877b89
MGough 22 <gentianloom@gmail.com>
2024-11-26 01:50:44 +0000

    initial tests and get endpoint for all articles with specific sorting conditions

--- models/api.model.js
+++ models/api.model.js
@@ -12,3 +12,23 @@ exports.fetchArticleById = async id => {
   );
   return rows[0];
 };
+
+exports.fetchAllArticles = async () => {
+  const { rows } = await db.query("SELECT * FROM articles;");
+  const result = rows
+    .map(({ body, ...rest }) => rest)
+    .sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+  const countVotes = async id => {
+    const { rows } = await db.query(
+      "SELECT SUM(votes) FROM comments WHERE article_id = $1;",
+      [id]
+    );
+    return rows[0].sum || 0;
+  };
+  await Promise.all(
+    result.map(async el => {
+      el.comment_count = Number(await countVotes(el.article_id));
+    })
+  );
+  return result;
+};
